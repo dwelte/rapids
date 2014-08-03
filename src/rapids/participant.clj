bbd4f48be25370e0903a7c00d8669fd396497605
David Welte <dcw6@cornell.edu>
2014-08-03 23:01:05 +0000

    Begin work on server logic
    Begin work on the server logic starting with the AppendEntries RPC.

---
+++ src/rapids/participant.clj
@@ -0,0 +1,52 @@
+(ns rapids.participant)
+
+(defrecord ServerState
+  [; persistant
+   current-term
+   voted-for
+   logs
+
+   ; volatile
+   commit-index
+   last-applied
+   next-indexs
+   match-indexs
+
+   expiration-time
+   role
+   state-machine])
+
+(defn persist-write-entries
+  [server-state {:keys [term
+                        leader-id
+                        prev-log-index
+                        prev-log-term
+                        entries
+                        leader-commit]
+                 :as    arguments}]
+  (-> server-state
+      (trim-log-to prev-log-index)
+      (persist-append-entries prev-log-index entries)
+      (update-commit-index leader-commit)))
+
+(defn write-entries-process
+  [server-state {:keys [term
+                        leader-id
+                        prev-log-index
+                        prev-log-term
+                        entries
+                        leader-commit]
+                 :as    arguments}]
+  (cond
+    (< term (server-state :current-term))               [false server-state]
+    (not= (-> logs prev-log-index :term) prev-log-term) [false server-state]
+    :default                                            [true  (persist-write-entries server-state argmuents)]))
+
+(defn append-entries
+  [server-state-atom arguments]
+  (let [server-state              @server-state-atom
+        [result new-server-state] (append-entries-process server-state arguments)]
+    (reset! server-state new-server-state)
+    {:term    (new-server-state :current-term)
+     :success result}))
+
